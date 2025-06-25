e9487ed2682abf8846ffafc3d417f806b6504626
SpiritSK <devonsebastiankoswara@gmail.com>
2025-06-25 23:57:14 +0700

    Added reusable component for deal cards

---
+++ app/screens/Deals/components/DealCard.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from "react-native";
+
+import { Deal } from '../../../types/Deal';
+import { calculateSavings } from '../../../utils/calculateSavings';
+
+export const DealCard = ({ deal, onPress }: { deal: Deal, onPress: () => void }) => {
+    return (
+        <TouchableOpacity
+            key={deal.dealID}
+            onPress={onPress}
+        >
+            <View className="bg-white rounded-xl p-4 mb-4 shadow">
+                <Text className="text-xl font-bold">{deal.dealName}</Text>
+                <Text className="text-sm text-gray-600">{deal.itemName}</Text>
+
+                <Text className="text-green-700 font-semibold">
+                    Save {calculateSavings(deal).toLocaleString()} SGD
+                </Text>
+
+                {deal.itemOrigPrice && (
+                    <Text className="text-sm text-gray-500 line-through">
+                        {deal.itemOrigPrice.toLocaleString()} SGD
+                    </Text>
+                )}
+
+                <Text className="text-sm text-blue-500">
+                    📍 {deal.dealStore}
+                </Text>
+            </View>
+        </TouchableOpacity>
+    );
+};
