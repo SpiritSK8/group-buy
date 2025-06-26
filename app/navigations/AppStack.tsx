660dd03ffb70f015d40ac280072ce0736d58d781
SpiritSK <devonsebastiankoswara@gmail.com>
2025-06-27 00:08:37 +0700

    Added type safety and icons for bottom nav bar

--- app/navigations/AppStack.tsx
+++ app/navigations/AppStack.tsx
@@ -1,26 +1,42 @@
 import React from "react";
-import { Button } from "react-native";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from "@expo/vector-icons";
 
-import { useAuth } from "../context/AuthContext";
-
-import DealsStack from "../navigations/DealsStack";
+import DealsStack from "./DealsStack";
 import GroupBuys from "../screens/(tabs)/GroupBuys";
-import Settings from "../screens/(tabs)/Settings";
+import Settings from "../screens/Settings/Settings";
 import ChatStack from "./ChatStack";
+import { AppStackParamList } from "../types/Navigations";
 
-const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator<AppStackParamList>();
 
 const AppStack = () => {
     return (
         <Tab.Navigator
-            screenOptions={{
+            screenOptions={({ route }) => ({
                 headerStyle: { backgroundColor: '#7766dd', height: 80 },
-            }}
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName: keyof typeof Ionicons.glyphMap;
+
+                    if (route.name === 'Deals') {
+                        iconName = focused ? 'pricetags' : 'pricetags-outline';
+                    } else if (route.name === 'GroupBuys') {
+                        iconName = focused ? 'people' : 'people-outline';
+                    } else if (route.name === 'Chats') {
+                        iconName = focused ? 'chatbubble' : 'chatbubble-outline';
+                    } else {
+                        iconName = focused ? 'settings' : 'settings-outline';
+                    }
+
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                },
+                tabBarActiveTintColor: '#7766dd',
+                tabBarInactiveTintColor: 'gray',
+            })}
         >
-            <Tab.Screen name='Deals' component={DealsStack} />
+            <Tab.Screen name='Deals' component={DealsStack}></Tab.Screen>
             <Tab.Screen name='GroupBuys' component={GroupBuys} />
-            <Tab.Screen name='Chats' component={ChatStack} options={{ headerShown: false }}/>
+            <Tab.Screen name='Chats' component={ChatStack} options={{ headerShown: false }} />
             <Tab.Screen name='Settings' component={Settings} />
         </Tab.Navigator>
     );
