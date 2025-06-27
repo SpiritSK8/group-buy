cc619b4852a16e34c618496d319ccb932f125b1a
SpiritSK <devonsebastiankoswara@gmail.com>
2025-06-27 21:04:59 +0700

    Added color constants

---
+++ app/constants/Colors.ts
@@ -0,0 +1,14 @@
+export const Colors = {
+    primary: '#378DDB',
+    secondary: '#82D173',
+    // accent: '#D84727',
+    error: '#D84727',
+    
+    light: {
+        text: '#001021',
+        background: '#D9DEEC',
+        navBackground: '#E8E9F3',
+        iconInactive: '#D9DEEC',
+        iconActive: '#378DDB',
+    }
+}
