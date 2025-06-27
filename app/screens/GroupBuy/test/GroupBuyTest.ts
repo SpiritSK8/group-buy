7ee0a0134a1f3fccabe8f66a99243ce9007ff879
SpiritSK <devonsebastiankoswara@gmail.com>
2025-06-28 01:24:33 +0700

    Added test cases for group buy

---
+++ app/screens/GroupBuy/test/GroupBuyTest.ts
@@ -0,0 +1,11 @@
+export const GROUP_BUY_TEST_DATA = [
+    {id: "g1"},
+    {id: "g2"},
+    {id: "g3"},
+    {id: "g4"},
+    {id: "g5"},
+    {id: "g6"},
+    {id: "g7"},
+    {id: "g8"},
+    {id: "g9"}
+]
