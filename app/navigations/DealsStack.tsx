beba4197160dc35b88e7c58eb1928f51b5897ec4
SpiritSK <devonsebastiankoswara@gmail.com>
2025-07-25 03:02:08 +0700

    Changed indentation into 4 spaces

--- app/navigations/DealsStack.tsx
+++ app/navigations/DealsStack.tsx
@@ -4,17 +4,25 @@ import Deals from '../screens/Deals/Deals';
 import DealContributionForm from '../screens/Deals/DealContributionForm';
 import NewDealForm from '../screens/Deals/NewDealForm';
 import { DealStackParamList } from '../types/Navigations';
+import { Colors } from '../constants/Colors';
 
 const Stack = createNativeStackNavigator<DealStackParamList>();
 
 const DealsStack = () => {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="DealsHome" component={Deals} options={{ title: 'Deals' }} />
-      <Stack.Screen name="DealContributionForm" component={DealContributionForm} options={{ title: 'Contribute to Deal' }} />
-      <Stack.Screen name="NewDealForm" component={NewDealForm} />
-    </Stack.Navigator>
-  );
+    return (
+        <Stack.Navigator
+            initialRouteName="DealsHome"
+            screenOptions={{
+                headerShown: true,
+                title: 'Chat',
+                headerStyle: { backgroundColor: Colors.primary }
+            }}
+        >
+            <Stack.Screen name="DealsHome" component={Deals} options={{ title: 'Deals' }} />
+            <Stack.Screen name="DealContributionForm" component={DealContributionForm} options={{ title: 'Contribute to Deal' }} />
+            <Stack.Screen name="NewDealForm" component={NewDealForm} />
+        </Stack.Navigator>
+    );
 };
 
 export default DealsStack;
