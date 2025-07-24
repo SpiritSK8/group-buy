e9b7bf11c278dc58f8ee3d7174f4740cca609448
SpiritSK <devonsebastiankoswara@gmail.com>
2025-07-25 03:05:39 +0700

    Added contributions to GroupBuys

--- app/types/GroupBuyTypes.ts
+++ app/types/GroupBuyTypes.ts
@@ -1,6 +1,12 @@
+export type Contribution = {
+    userUID: string;
+    amount: number;
+};
+
 export interface GroupBuyDetails {
     id: string;
     dealID: string;
     participants: string[];
+    contributions: Contribution[];
     chatRoomID: string;
 }
